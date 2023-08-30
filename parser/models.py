e82bb5ef91236f633f36d3b22ba2c04d6e861734
satel31 <blokhnina.tatiana@yandex.ru>
2023-08-31 00:52:43 +0300

    Created model problem

---
+++ parser/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Problem(models.Model):
+    topics = models.TextField(verbose_name='Topics')
+    solutions_amount = models.IntegerField(**NULLABLE, verbose_name='Solutions amount')
+    problem_name = models.CharField(max_length=255, verbose_name='Problem name')
+    index = models.CharField(max_length=100, verbose_name='Index')
+    rating = models.IntegerField(**NULLABLE, verbose_name='rating')
+
+    def __str__(self):
+        return f'Problem: {self.problem_name}'
+
+    class Meta:
+        verbose_name = 'problem'
+        verbose_name_plural = 'problems'
